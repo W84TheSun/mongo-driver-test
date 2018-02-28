99d6b9ac3767bee81da168ee6ec1f785eada1e17
Dmitry <wait4thesun@gmail.com>
2018-02-28 20:26:47 +0300

    Count example

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"fmt"
+	"context"
+	"github.com/mongodb/mongo-go-driver/bson"
+)
+
+func main() {
+	client, err := mongo.NewClient("mongodb://localhost")
+	if err != nil {
+		fmt.Println(err)
+	}
+	db := client.Database("test")
+
+	collection := db.Collection("testColl")
+
+	result, err := collection.InsertOne(
+		context.Background(),
+		bson.NewDocument(
+			bson.EC.String("item", "canvas"),
+			bson.EC.Int32("qty", 100),
+		))
+
+	fmt.Printf("%#v\n", result)
+	fmt.Printf("%#v\n", result.InsertedID)
+
+	fmt.Println(collection.Count(context.Background(), bson.NewDocument(
+		bson.EC.Int32("qty", 100),
+	)))
+}
